6e94e6813ee84f237574f8cdda15c526cc0f2182
theo.mottet <theo.mottet@epitech.eu>
2018-02-14 13:42:25 +0100

    shared lib

---
+++ inc/IModule.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "api/Http.hpp"
+
+class IModule
+{
+    public:
+        IModule(int priority): priority(priority) {};
+        virtual void exec(const zia::api::HttpDuplex&);
+        int getPriority() const {return this->priority;};
+    private:
+        unsigned int priority;
+};
