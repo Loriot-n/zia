2d9906b658179e5a2ffe53d8ec38f7ba2fb6bd2e
melis_m <matteo.melis@epitech.eu>
2018-02-23 22:30:50 +0100

    moved lots of code to new functions for easier reading, now checks if index.html exists when opening a dir, and opens it if it exists

--- modules/file_reader/FileReader.cpp
+++ modules/file_reader/FileReader.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
-#include <boost/filesystem.hpp>
 #include "FileReader.hpp"
 
 namespace fs = boost::filesystem;
@@ -11,67 +10,17 @@ extern "C" IModule *create()
   return new FileReader();
 }
 
-void FileReader::putStringToResp(std::string const &str, zia::api::HttpResponse &resp)
-{
-  std::byte const *bytes = reinterpret_cast<std::byte const *>(str.c_str());
-  resp.body.insert(resp.body.end(), bytes, bytes + str.size());
-}
-
 bool FileReader::exec(zia::api::HttpDuplex &dup)
 {
-  for (std::byte b : dup.raw_req)
-    {
-      std::cout << static_cast<char>(b);
-    }
-  std::cout << std::endl;
   fs::path const target = fs::path(rootPath) / fs::path(dup.req.uri);
   std::cout << "Requesting file " << rootPath << " / " << dup.req.uri << std::endl;
   if (fs::is_directory(target))
     {
-      dup.resp.headers.emplace(std::make_pair("Content-Type", "text/html"));
-
-      std::stringstream top;
-      top <<
-	"<!DOCTYPE html>\n"
-	"<html lang=\"en_UK\">\n"
-	"<head>\n"
-	"<meta charset=\"UTF-8\">\n"
-	"<title>Index of /" << target.filename().native() << "</title>\n"
-	"</head>\n"
-	"<body>\n"
-	"<table>\n"
-	"<tr><td><a href=\"" << "../\">" << "../</a></td></tr>\n";
-      putStringToResp(top.str(), dup.resp);
-      for (fs::directory_entry &entry : fs::directory_iterator(target))
-	{
-	  std::cout << entry.path() << std::endl;
-	  std::stringstream ss;
-	  ss << "<tr><td><a href=\"" << entry.path().filename().native();
-	  if (fs::is_directory(entry))
-	    ss << '/';
-	  ss << "\">" << entry.path().filename().c_str();
-	  if (fs::is_directory(entry))
-	    ss << '/';
-	  ss << "</a></td></tr>\r\n";
-	  putStringToResp(ss.str(), dup.resp);
-	}
-      putStringToResp(
-	"</table>\n"
-	"</body>\n"
-	"</html>\n",
-	dup.resp);
-    }
-  else if (fs::ifstream inputStream{target,
-      std::ios::binary | std::ios::ate})
-    {
-      int size = inputStream.tellg();
-      inputStream.seekg(0);
-      dup.resp.body.resize(size);
-      inputStream.read(reinterpret_cast<char *>(dup.resp.body.data()), size);
+      handleDir(target, dup);
     }
   else
     {
-      std::cerr << "Couldn't find file " << target << std::endl;
+      handleFile(target, dup);
     }
   return true;
 }
@@ -85,4 +34,88 @@ bool FileReader::config(zia::api::Conf const &conf)
       return true;
     }
   return false;
+}
+
+void FileReader::putStringToResp(std::string const &str, zia::api::HttpResponse &resp)
+{
+  std::byte const *bytes = reinterpret_cast<std::byte const *>(str.c_str());
+  resp.body.insert(resp.body.end(), bytes, bytes + str.size());
+}
+
+void FileReader::addHeader(zia::api::HttpDuplex &dup,
+			   std::string const &key, std::string const &value)
+{
+  dup.resp.headers.emplace(std::make_pair(key, value));
+
+}
+
+void FileReader::handleDir(fs::path const &target, zia::api::HttpDuplex &dup)
+{
+  addHeader(dup, "Content-Type", "text/html");
+  if (hasIndexHtml(target))
+    {
+      return handleFile(target / "index.html", dup);
+    }
+
+  std::stringstream top;
+  top <<
+    "<!DOCTYPE html>\n"
+    "<html lang=\"en_UK\">\n"
+    "<head>\n"
+    "<meta charset=\"UTF-8\">\n"
+    "<title>Index of /" << target.filename().native() << "</title>\n"
+    "</head>\n"
+    "<body>\n"
+    "<table>\n"
+    "<tr><td><a href=\"" << "../\">" << "../</a></td></tr>\n";
+  putStringToResp(top.str(), dup.resp);
+  for (fs::directory_entry &entry : fs::directory_iterator(target))
+    {
+      std::cout << entry.path() << std::endl;
+      std::stringstream ss;
+      ss << "<tr><td><a href=\"" << entry.path().filename().native();
+      if (fs::is_directory(entry))
+	ss << '/';
+      ss << "\">" << entry.path().filename().c_str();
+      if (fs::is_directory(entry))
+	ss << '/';
+      ss << "</a></td></tr>\r\n";
+      putStringToResp(ss.str(), dup.resp);
+    }
+  putStringToResp(
+    "</table>\n"
+    "</body>\n"
+    "</html>\n",
+    dup.resp);
+}
+
+void FileReader::handleFile(fs::path const &target, zia::api::HttpDuplex &dup)
+{
+  if (fs::ifstream inputStream{target, std::ios::binary})
+    {
+      dup.resp.headers.emplace(std::make_pair("Content-Type", "text/plain"));
+      std::uintmax_t size = fs::file_size(target);
+      dup.resp.body.resize(size);
+      inputStream.read(reinterpret_cast<char *>(dup.resp.body.data()), size);
+    }
+  else
+    {
+      handleFileError(target, dup);
+    }
+}
+
+bool FileReader::hasIndexHtml(boost::filesystem::path const &targetDir) const
+{
+  for (fs::directory_entry &entry : fs::directory_iterator(targetDir))
+    {
+      std::cout << "checking " << entry.path().filename().native() << std::endl;
+      if (entry.path().filename().native() == "index.html")
+	return true;
+    }
+  return false;
+}
+
+void FileReader::handleFileError(fs::path const &target, zia::api::HttpDuplex &)
+{
+  std::cerr << target << " not found" << std::endl;
 }
--- modules/file_reader/FileReader.hpp
+++ modules/file_reader/FileReader.hpp
@@ -1,18 +1,27 @@
 #pragma once
 
 #include <iostream>
+#include <boost/filesystem.hpp>
 #include "IModule.hpp"
+
+namespace fs = boost::filesystem;
 
 class FileReader : public IModule
 {
-private:
-  std::string rootPath;
-
-  void putStringToResp(std::string const &, zia::api::HttpResponse &);
-
 public:
   FileReader() : IModule(5) {};
 
   bool exec(zia::api::HttpDuplex &) override;
   bool config(zia::api::Conf const &) override;
+
+private:
+  std::string rootPath;
+
+  void putStringToResp(std::string const &, zia::api::HttpResponse &);
+  void addHeader(zia::api::HttpDuplex &, std::string const &key, std::string const &value);
+
+  void handleDir(fs::path const &target, zia::api::HttpDuplex &);
+  void handleFile(fs::path const &target, zia::api::HttpDuplex &);
+  bool hasIndexHtml(fs::path const &targetDir) const;
+  void handleFileError(fs::path const &target, zia::api::HttpDuplex &);
 };
