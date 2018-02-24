c408a21d2179cfb2c7e8b3261ddb9830d456ac3d
Queenie <flora.huot@epitech.eu>
2018-02-24 19:43:34 +0100

    [ModuleLog] Fixed. [UML] Added.

--- modules/log/ModuleLog.hpp
+++ modules/log/ModuleLog.hpp
@@ -8,26 +8,17 @@
 #include <fstream>
 #include <cstdarg>
 
-#define LOG zia::ModuleLog::getLog()
-
 namespace zia {
-
-	enum logLevel {
-		DEBUG,
-		INFO,
-		WARNING,
-		ERROR
-	};
-
-	class ModuleLog : public zia::api::Module {
+	
+	class ModuleLog : public IModule {
 	private:
-		ModuleLog();
+		ModuleLog() : IModule(0) {};
 
 		static std::string const _logName;
 		static std::ofstream _logFile;
 
 	public:
-		bool config(zia::api::Conf const &);
-		bool exec(zia::api::HttpDuplex &);
+		bool config(zia::api::Conf const &) override;
+		bool exec(zia::api::HttpDuplex &) override;
 	};
 };
